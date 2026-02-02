89db491f612f9448c4c36442deb60f06ca08f02e
ddaog2 <ktu6593@naver.com>
2026-02-03 00:05:06 +0900

    fix: add missing DeckPage import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { DeckPage } from './pages/DeckPage';
 import { ResultPage } from './pages/ResultPage';
 
 function App() {
